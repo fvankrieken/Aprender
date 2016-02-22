2f0654e1c2687d7431e8a7b1e5f61fb545ccb0da
Finn van Krieken <finnvankrieken@Finns-MacBook-Pro.local>
2016-02-22 01:01:40 -0800

    Added index and RT

--- src/public/app/Controllers/EspController.js
+++ src/public/app/Controllers/EspController.js
@@ -1,4 +1,9 @@
 app.controller('EspController', ['$scope', function($scope) {
+//	$scope.remove = function(item) { 
+// 	 var index = $scope.ofertas.indexOf(item);
+// 	 $scope.ofertas.splice(index, 1);     
+//	}
 	$scope.ofertas = [
+
 ]
 }]);
