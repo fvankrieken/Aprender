06ef063e149466023be6f520356f8a412c722da0
Finn van Krieken <finnvankrieken@Finns-MacBook-Pro.local>
2016-02-23 15:57:40 -0800

    Bunch of changes in CdO for styling, plus navbar shadow

--- src/public/app/Controllers/EspController.js
+++ src/public/app/Controllers/EspController.js
@@ -5,5 +5,6 @@ app.controller('EspController', ['$scope', function($scope) {
 //	}
 	$scope.ofertas = [
 
+{ title: "Teorema de Pitagoras", descript: "But let's dispel with this fiction that Barack Obama doesn't know what he's doing: he knows exactly what he's doing. He is trying to make this country more like european countries and blahblahblah blah blah blah blahblah", filename: "CV.pdf"},
 ]
 }]);
