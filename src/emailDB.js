881649dcfc57fee02f2efecabe7b4332b957bbfb
Finn van Krieken <finnvankrieken@Finns-MacBook-Pro.local>
2016-03-02 19:13:54 -0800

    fix

--- src/emailDB.js
+++ src/emailDB.js
@@ -1,7 +1,5 @@
 var emailDB = {
 
-"gaiE": { "finn_van_krieken@alumni.brown.edu": "finnvankrieken@gmail.com", "finnvankrieken@gmail.com": "finn_van_krieken@alumni.brown.edu", "subject": "Teorema de Pitágoras, from: finn_van_krieken@alumni.brown.edu" },
-"I6lQ": { "finn_van_krieken@alumni.brown.edu": "finnvankrieken@gmail.com", "finnvankrieken@gmail.com": "finn_van_krieken@alumni.brown.edu", "subject": "Teorema de Pitágoras, from: finn_van_krieken@alumni.brown.edu" },
 };
 
 module.exports = emailDB;
