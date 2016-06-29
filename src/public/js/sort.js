bcc21f009b135768e6913095cc01387c74c686cf
Jessica Tong <Mongo@Jessica-Tongs-MacBook-Pro.local>
2016-06-29 12:15:16 -0700

    Angular in CdO

--- src/public/js/sort.js
+++ src/public/js/sort.js
@@ -1,6 +1,7 @@
 angular.module('sort', [])
 
 .controller('MainController', function($scope) {
+    $scope.sortTypeE = "order";
     $scope.sortTypeM = "order";
     $scope.sortTypeC = "order";
     $scope.sortTypeH = "order";
