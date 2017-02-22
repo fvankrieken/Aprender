d9a9484e8ce8ca1fabd210c89b4f24a05e656998
Finn van Krieken <finnvankrieken@Finns-MacBook-Pro.local>
2017-02-21 20:33:36 -0800

    temas

--- src/temas.js
+++ src/temas.js
@@ -365,8 +365,7 @@ MongoClient.connect(MongoURL, function(err, database) {
   	db = database;
 
   	for (var i = 0; i < temas.length; i++) {
-  		console.log('here')
-  		console.log(temas.length)
+  		console.log(tema.title)
 		tema = temas[i]
 		var collection = db.collection('temas');
 		tema.audio = ''
